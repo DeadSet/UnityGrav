05b624ae29e0e678119af0c81759a46189d299d7
DeadSet <DeadSet@users.noreply.github.com>
2017-09-04 00:08:45 -0500

    Added Rotation

--- Grav/Assets/Scripts/Thrust.cs
+++ Grav/Assets/Scripts/Thrust.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Thrust : MonoBehaviour {
 
     public float ThrustMagnitude = 1f;
+    public float TurnGain = 1f;
 
     private Rigidbody rb;
     private bool ThrustRequest = false;
+    private float TorqueRequest;
+    private bool TurnRequest = false;
 
 
 	// Use this for initialization
@@ -17,7 +20,7 @@ public class Thrust : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.anyKey)
+        if (Input.GetButton("Jump"))
         {
             ThrustRequest = true;
         }
@@ -25,13 +28,30 @@ public class Thrust : MonoBehaviour {
         {
             ThrustRequest = false;
         }
+
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) >= .1f)
+        {
+            TorqueRequest = Input.GetAxis("Horizontal");
+            TurnRequest = true;
+
+        }
+        else
+        {
+            TurnRequest = false;
+        }
 	}
 
     private void FixedUpdate()
     {
         if (ThrustRequest)
         {
-            rb.AddForce(Vector3.left * ThrustMagnitude);
+            rb.AddRelativeForce(Vector3.up * ThrustMagnitude);
+            
+        }
+
+        if (TurnRequest)
+        {
+            rb.transform.Rotate(TorqueRequest * Time.deltaTime * TurnGain, 0, 0, Space.Self);
         }
     }
 }
