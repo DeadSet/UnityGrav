5eaff824fe978f2de7335e4a4bcb7d29d2ba08a8
DeadSet <DeadSet@users.noreply.github.com>
2017-09-04 13:03:21 -0500

    inprocess mouse turning

--- Grav/Assets/Scripts/Thrust.cs
+++ Grav/Assets/Scripts/Thrust.cs
@@ -9,13 +9,16 @@ public class Thrust : MonoBehaviour {
 
     private Rigidbody rb;
     private bool ThrustRequest = false;
-    private float TorqueRequest;
-    private bool TurnRequest = false;
+    //private float TorqueRequest;
+    //private bool TurnRequest = false;
+    private int rayTarget;
+    private float rayLength = 1000f;
 
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
         rb = GetComponent<Rigidbody>();
+        rayTarget = LayerMask.GetMask("RayTarget");
 	}
 	
 	// Update is called once per frame
@@ -28,7 +31,7 @@ public class Thrust : MonoBehaviour {
         {
             ThrustRequest = false;
         }
-
+/*
         if (Mathf.Abs(Input.GetAxis("Horizontal")) >= .1f)
         {
             TorqueRequest = Input.GetAxis("Horizontal");
@@ -39,19 +42,40 @@ public class Thrust : MonoBehaviour {
         {
             TurnRequest = false;
         }
+*/
 	}
 
     private void FixedUpdate()
     {
         if (ThrustRequest)
         {
-            rb.AddRelativeForce(Vector3.up * ThrustMagnitude);
-            
+            rb.AddRelativeForce(Vector3.up * ThrustMagnitude); 
         }
-
+        /*
         if (TurnRequest)
         {
             rb.transform.Rotate(TorqueRequest * Time.deltaTime * TurnGain, 0, 0, Space.Self);
         }
+        */
+
+        Turn();
+    }
+
+    void Turn()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Debug.Log(Input.mousePosition);
+        
+
+        if (Physics.Raycast(ray, out hit, rayLength, rayTarget, QueryTriggerInteraction.Collide))
+        {
+            Vector3 thisToMouse = hit.point - transform.position;
+            thisToMouse.y = 0f;
+            Debug.DrawLine(transform.position, thisToMouse);
+            Quaternion newRotation = Quaternion.LookRotation(thisToMouse);
+            rb.MoveRotation(newRotation);
+        }
+        Debug.Log(Physics.Raycast(ray, out hit, rayLength, rayTarget, QueryTriggerInteraction.Collide));
     }
 }
