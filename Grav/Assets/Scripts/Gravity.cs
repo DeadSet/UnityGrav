d3ac985f7a36761c0fb9091e321bbdfe7c27f6dd
DeadSet <DeadSet@users.noreply.github.com>
2017-09-03 21:09:20 -0500

    Orbits!

---
+++ Grav/Assets/Scripts/Gravity.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+
+    public Rigidbody objToAttract;
+    public float G = 100f;
+
+
+    public Rigidbody rb;
+   
+    
+    
+    // Use this for initialization
+	void Awake () {
+
+      
+        
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Attract(objToAttract);
+	}
+
+    void Attract(Rigidbody objToAttract)
+    {
+        Rigidbody rbToAttract = objToAttract.GetComponent<Rigidbody>();
+
+        Vector3 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+
+        float forceMagnitude = Time.deltaTime * G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        rbToAttract.AddForce(force);
+    }
+}
---
+++ Grav/Assets/Scripts/Thrust.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrust : MonoBehaviour {
+
+    public float ThrustMagnitude = 1f;
+
+    private Rigidbody rb;
+    private bool ThrustRequest = false;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey)
+        {
+            ThrustRequest = true;
+        }
+        else
+        {
+            ThrustRequest = false;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        if (ThrustRequest)
+        {
+            rb.AddForce(Vector3.left * ThrustMagnitude);
+        }
+    }
+}
