b83129a8dda6a43e3c5019e1d0a0407dfb2a85af
DeadSet <DeadSet@users.noreply.github.com>
2017-09-10 20:57:57 -0500

    start camera controller

---
+++ Grav/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+
+    public float dampTime = .2f;
+    public float ScreenEdgeBuffer = 4f;
+    public float MinSize = 20f;
+    public float MaxSize = 80f;
+    public GameObject playerShip;
+
+    private Camera gameCamera;
+    private float ZoomSpeed;
+    
+
+
+	// Use this for initialization
+	void Awake () {
+        gameCamera = GetComponent<Camera>();
+        //playerShip = 
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Zoom();
+		
+	}
+
+    private void Zoom()
+    {
+       
+    }
+}
