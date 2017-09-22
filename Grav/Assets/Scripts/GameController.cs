1774b0789bef38131221443975fe84645879199a
DeadSet <DeadSet@users.noreply.github.com>
2017-09-21 21:33:47 -0500

    Added Random Spawn Ring

--- Grav/Assets/Scripts/GameController.cs
+++ Grav/Assets/Scripts/GameController.cs
@@ -7,8 +7,9 @@ public class GameController : MonoBehaviour {
     public GameObject playerShip;
     public Vector3 startingForce;
     public Vector3 spawnPosition;
+    public float spawnRingRadius;
     //for testing
-    public bool spawnThreat;
+    public bool RequestThreat;
     public GameObject Threat;
 
 
@@ -23,12 +24,23 @@ public class GameController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (spawnThreat)
+        if (RequestThreat)
         {
-            Instantiate(Threat, new Vector3(20f, 0f, 0f), Quaternion.identity);
-            spawnThreat = false;
+            SpawnThreat();
+            RequestThreat = false;
         }
         
 		
 	}
+
+    void SpawnThreat()
+    {
+        float x;
+        float y;
+
+        x = Random.Range(-spawnRingRadius, spawnRingRadius);
+        y = Mathf.Sqrt(Mathf.Pow(spawnRingRadius, 2f) - Mathf.Pow(x, 2f));
+        y = y *(Random.Range(0, 2) * 2 - 1); //returns 1 or -1, y will allways start positive
+        Instantiate(Threat, new Vector3(x, 0f, y), Quaternion.identity);
+    }
 }
